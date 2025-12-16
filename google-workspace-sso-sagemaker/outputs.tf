3c099f5e4f7d92b3eb00697d8eeb327d89b1b91d
herley <herley.shaori@gmail.com>
2025-12-16 21:16:23 +0700

    Remove IAM user, prepare for SSO integration

--- google-workspace-sso-sagemaker/outputs.tf
+++ google-workspace-sso-sagemaker/outputs.tf
@@ -13,12 +13,3 @@ output "data_scientist_ou_arn" {
   value       = aws_organizations_organizational_unit.data_scientist.arn
 }
 
-output "data_scientist_user_name" {
-  description = "Name of the Data Scientist IAM user"
-  value       = aws_iam_user.data_scientist.name
-}
-
-output "data_scientist_user_arn" {
-  description = "ARN of the Data Scientist IAM user"
-  value       = aws_iam_user.data_scientist.arn
-}
