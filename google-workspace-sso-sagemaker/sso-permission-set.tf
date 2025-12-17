3e4794c01fc6ce19f10860fa255a6b7dbc53e69d
herley <herleys@xlsmart.co.id>
2025-12-17 09:11:41 +0700

    Add SageMaker domain access permissions to Data Scientist permission set

--- google-workspace-sso-sagemaker/sso-permission-set.tf
+++ google-workspace-sso-sagemaker/sso-permission-set.tf
@@ -13,6 +13,33 @@ resource "aws_ssoadmin_managed_policy_attachment" "data_scientist_readonly" {
   managed_policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
 
+resource "aws_ssoadmin_permission_set_inline_policy" "data_scientist_sagemaker" {
+  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  permission_set_arn = aws_ssoadmin_permission_set.data_scientist.arn
+
+  inline_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "SageMakerDomainAccess"
+        Effect = "Allow"
+        Action = [
+          "sagemaker:ListDomains",
+          "sagemaker:DescribeDomain",
+          "sagemaker:ListApps",
+          "sagemaker:DescribeApp",
+          "sagemaker:ListUserProfiles",
+          "sagemaker:DescribeUserProfile",
+          "sagemaker:CreatePresignedDomainUrl",
+          "sagemaker:ListSpaces",
+          "sagemaker:DescribeSpace"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 data "aws_identitystore_group" "data_scientist" {
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
