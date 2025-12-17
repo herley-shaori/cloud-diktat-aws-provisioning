68a1088b6cf11f90cee9ffcb3c663fa4a2b63417
herley <herleys@xlsmart.co.id>
2025-12-17 10:23:02 +0700

    Add Identity Store user, group, and membership for data-scientist@cloud-diktat.info

--- google-workspace-sso-sagemaker/sso.tf
+++ google-workspace-sso-sagemaker/sso.tf
@@ -5,26 +5,49 @@ data "aws_caller_identity" "current" {}
 resource "aws_ssoadmin_permission_set" "data_scientist" {
   name             = "DataScientist"
   description      = "Permission set for Data Scientists with SageMaker and S3 access"
-  instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
+  instance_arn     = data.aws_ssoadmin_instances.this.arns[0]
   session_duration = "PT8H"
 }
 
 resource "aws_ssoadmin_managed_policy_attachment" "sagemaker_full_access" {
-  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  managed_policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+  instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
   permission_set_arn = aws_ssoadmin_permission_set.data_scientist.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
 }
 
 resource "aws_ssoadmin_managed_policy_attachment" "s3_read_only" {
-  instance_arn       = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  managed_policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
   permission_set_arn = aws_ssoadmin_permission_set.data_scientist.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_identitystore_user" "data_scientist" {
+  identity_store_id = data.aws_ssoadmin_instances.this.identity_store_ids[0]
+
+  user_name    = "data-scientist@cloud-diktat.info"
+  display_name = "Data Scientist"
+
+  name {
+    given_name  = "Data"
+    family_name = "Scientist"
+  }
+
+  emails {
+    value   = "data-scientist@cloud-diktat.info"
+    primary = true
+  }
 }
 
 resource "aws_identitystore_group" "data_scientist" {
   identity_store_id = data.aws_ssoadmin_instances.this.identity_store_ids[0]
-  display_name      = "data-scientist@cloud-diktat.info"
-  description       = "Data Scientists group from Google Workspace"
+  display_name      = "DataScientists"
+  description       = "Data Scientists group"
+}
+
+resource "aws_identitystore_group_membership" "data_scientist" {
+  identity_store_id = data.aws_ssoadmin_instances.this.identity_store_ids[0]
+  group_id          = aws_identitystore_group.data_scientist.group_id
+  member_id         = aws_identitystore_user.data_scientist.user_id
 }
 
 resource "aws_ssoadmin_account_assignment" "data_scientist_assignment" {
