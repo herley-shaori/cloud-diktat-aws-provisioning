c13c6e64724b0fec5dead4c5707b3b4313a0cca2
herley <herleys@xlsmart.co.id>
2025-12-17 09:50:29 +0700

    Replace deprecated filter block with alternate_identifier in aws_identitystore_group

--- google-workspace-sso-sagemaker/sso.tf
+++ google-workspace-sso-sagemaker/sso.tf
@@ -18,3 +18,25 @@ resource "aws_ssoadmin_managed_policy_attachment" "s3_read_only" {
   managed_policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   permission_set_arn = aws_ssoadmin_permission_set.data_scientist.arn
 }
+
+data "aws_identitystore_group" "data_scientist" {
+  identity_store_id = data.aws_ssoadmin_instances.this.identity_store_ids[0]
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = "data-scientist@cloud-diktat.info"
+    }
+  }
+}
+
+resource "aws_ssoadmin_account_assignment" "data_scientist_assignment" {
+  instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
+  permission_set_arn = aws_ssoadmin_permission_set.data_scientist.arn
+
+  principal_id   = data.aws_identitystore_group.data_scientist.group_id
+  principal_type = "GROUP"
+
+  target_id   = "623127157773" # YOUR AWS ACCOUNT ID
+  target_type = "AWS_ACCOUNT"
+}
