0a6d10256437cf2be73b8b68308bf4659c7cbe0c
herley <herley.shaori@gmail.com>
2025-12-19 20:43:16 +0700

    Add AWS profile pribadi to coffee-shop-bedrock provider

--- coffee-shop-bedrock/main.tf
+++ coffee-shop-bedrock/main.tf
@@ -29,7 +29,8 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
+  profile = "pribadi"
 }
 
 # =============================================================================
