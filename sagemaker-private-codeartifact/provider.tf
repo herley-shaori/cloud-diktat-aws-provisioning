772dfe4afb574d9b6582fa26772d84dcf4dd8f93
herley <herleys@xlsmart.co.id>
2025-12-19 10:02:30 +0700

    Update region.

--- sagemaker-private-codeartifact/provider.tf
+++ sagemaker-private-codeartifact/provider.tf
@@ -1,4 +1,11 @@
 provider "aws" {
-  region  = var.aws_region
+  region  = "ap-southeast-1"
   profile = "pribadi"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      ManagedBy   = "Terraform"
+    }
+  }
 }
